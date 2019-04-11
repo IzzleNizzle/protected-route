a76b151b2d90dbc9094a7720f6068eec09527f16
ctrlz <pehisaac@gmail.com>
2019-04-11 00:27:47 -0600

    Tweek for Github pages to work with React Router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
 
-ReactDOM.render(<BrowserRouter>
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}>
   <App />
 </BrowserRouter>, document.getElementById('root'));
